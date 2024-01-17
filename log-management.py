6f4a99fcd96cf0546d8dae4ab631103b9c189a52
cfff226 <cazziednb@outlook.com>
2024-01-17 17:55:12 +0000

    Add Pandas to read to text file into data frame - Use Pandas to read the users choice of log file and separate the data by ';' - Assign all data to a variable log data - Output the log data to the user using the Pandas data frame

--- log-management.py
+++ log-management.py
@@ -1,6 +1,9 @@
 # A log management tool that reads shipping logs saved as text files
 # And displays them to a user in a presentable and readable fashion
 
+import pandas as pd
+
+
 list_of_files = ["sep-23.txt", "oct-23.txt", "nov-23.txt", "dec-23.txt"]
 
 
@@ -19,9 +22,10 @@ def main():
             input("Please enter the name of the text file that you wish to view: \n")
         )
         if choice in list_of_files:
-            with open(str(choice), "r") as log_file:
-                log_data = log_file.read().split("\n")
-                print(log_data)
+            # read text file into pandas DataFrame
+            log_data = pd.read_table(choice, sep=";")
+            print(f"\n{log_data}\n")
+
         else:
             print("\n\t\t\tYour choice has not been recognised\n")
             main()
