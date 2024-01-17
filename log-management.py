7d3b9f27ea87d867e05fbcb7db226738351bcfe3
cfff226 <cazziednb@outlook.com>
2024-01-17 15:38:17 +0000

    Declare list of strings which are each of the text files

--- log-management.py
+++ log-management.py
@@ -1,5 +1,6 @@
 # A log management tool that reads shipping logs saved as text files
 # And displays them to a user in a presentable and readable fashion
 
+list_of_files = ["sep-23.txt", "oct-23.txt", "nov-23.txt", "dec-23.txt"]
 
-choice = str(input("Please enter the name of the text file that you wish to view"))
+choice = str(input("Please enter the name of the text file that you wish to view: "))
