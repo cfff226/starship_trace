565bf0df1f7d644cf1a0be4a110f8babb377da2c
cfff226 <cazziednb@outlook.com>
2024-01-17 15:24:23 +0000

    Initial commit - Add input function to request the choice of text files from the user and assign this to a variable called choice

---
+++ log-management.py
@@ -0,0 +1,5 @@
+# A log management tool that reads shipping logs saved as text files
+# And displays them to a user in a presentable and readable fashion
+
+
+choice = str(input("Please enter the name of the text file that you wish to view"))
