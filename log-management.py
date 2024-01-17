37ec730e4cfa3b0e234071de9711bac9ac84f7ae
cfff226 <cazziednb@outlook.com>
2024-01-17 16:53:16 +0000

    Add with open statement to read log file of users choice - Split the data by a new line - Output this to the user

--- log-management.py
+++ log-management.py
@@ -3,18 +3,30 @@
 
 list_of_files = ["sep-23.txt", "oct-23.txt", "nov-23.txt", "dec-23.txt"]
 
+
 # Add item to cart
 def main():
-    print("----------------------------------------------------------------------------------\n\t\t\t\t\tMENU\n")
+    print(
+        "----------------------------------------------------------------------------------\n\t\t\t\t\tMENU\n"
+    )
     for i in range(len(list_of_files)):
         print(str(i + 1) + ". " + list_of_files[i])
-    print("\n----------------------------------------------------------------------------------\n")
+    print(
+        "\n----------------------------------------------------------------------------------\n"
+    )
     try:
-        choice = str(input("Please enter the name of the text file that you wish to view: "))
+        choice = str(
+            input("Please enter the name of the text file that you wish to view: \n")
+        )
+        with open(str(choice), "r") as log_file:
+            log_data = log_file.read().split("\n")
+            print(log_data)
         if choice not in list_of_files:
-            print("Your choice has not been recognised")
+            print("\n\t\t\tYour choice has not been recognised\n")
             main()
     except ValueError:
-        print("You have entered an incorrect value")
-   
+        print("\n\t\t\tYou have entered an incorrect value\n")
+        main()
+
+
 main()
