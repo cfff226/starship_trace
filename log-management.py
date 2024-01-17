68f002d57700e3af0e64ede692b35043e59f73ae
cfff226 <cazziednb@outlook.com>
2024-01-17 15:44:49 +0000

    Define function called main to show the main menu - Loop through the range, length of list of files - Output an enumerated list of options for the user to choose from - Call the main function

--- log-management.py
+++ log-management.py
@@ -3,4 +3,11 @@
 
 list_of_files = ["sep-23.txt", "oct-23.txt", "nov-23.txt", "dec-23.txt"]
 
-choice = str(input("Please enter the name of the text file that you wish to view: "))
+# Add item to cart
+def main():
+    print("----------------------------------------------------------------------------------\n\t\t\t\t\tMENU\n")
+    for i in range(len(list_of_files)):
+        print(str(i + 1) + ". " + list_of_files[i])
+    print("\n----------------------------------------------------------------------------------\n")
+   
+main()
